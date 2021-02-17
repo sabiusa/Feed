db0cb2ba9a77db5aebf30e520096ab3d7abd8dab
Saba Khutsishvili <skhutsishvili@bog.ge>
2021-02-17 14:35:24 +0400

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Saba Khutsishvili on 2/17/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
