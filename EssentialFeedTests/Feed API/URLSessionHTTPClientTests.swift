b106fd8b73bb3a9a5be3fd64c33e6c27eadfc147
Saba Khutsishvili <skhutsishvili@bog.ge>
2021-02-23 19:49:07 +0400

    Use factory method to create error

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -59,7 +59,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let requestError = NSError(domain: "Test Error", code: 1)
+        let requestError = anyNSError()
         
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
         
