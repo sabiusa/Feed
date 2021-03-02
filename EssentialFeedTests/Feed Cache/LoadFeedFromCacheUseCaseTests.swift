34acedb581e64846889778c2c9391fe9cf9f8cb9
sabiusa <sabakhuts@gmail.com>
2021-03-02 22:49:50 +0400

    Deletes cache on retrieval error

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -82,6 +82,15 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
             )
         })
     }
+    
+    func test_load_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
 
     // MARK:- Helpers
     
