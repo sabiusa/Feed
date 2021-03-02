672d2907f8b1a446cf9700a58e34595f4963d1e4
sabiusa <sabakhuts@gmail.com>
2021-03-02 09:47:26 +0400

    Move `LocalFeedItem` to separate file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,27 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Saba Khutsishvili on 3/2/21.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(
+        id: UUID,
+        description: String?,
+        location: String?,
+        imageURL: URL
+    ) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
