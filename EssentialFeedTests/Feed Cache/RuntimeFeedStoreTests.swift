613c520eb294c28b7d4cf69b5501ba412911f71b
sabiusa <sabakhuts@gmail.com>
2021-03-09 21:20:37 +0400

    Create `RuntimeFeedStoreTest` testing class

---
+++ EssentialFeedTests/Feed Cache/RuntimeFeedStoreTests.swift
@@ -0,0 +1,59 @@
+//
+//  RuntimeFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Saba Khutsishvili on 3/9/21.
+//
+
+import XCTest
+
+class RuntimeFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviousInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+}
