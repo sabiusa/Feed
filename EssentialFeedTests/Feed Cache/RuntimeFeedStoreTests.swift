e14e4776bb1ff93950866adc96447fe2fb3611a5
sabiusa <sabakhuts@gmail.com>
2021-03-09 21:35:55 +0400

    Assert that retrieve delivers found values on non-empty cache

--- EssentialFeedTests/Feed Cache/RuntimeFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/RuntimeFeedStoreTests.swift
@@ -10,12 +10,24 @@ import EssentialFeed
 
 class RuntimeFeedStore: FeedStore {
     
+    private struct Cache {
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+    
+    private var cache: Cache?
+    
     func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        guard let cache = cache, !cache.feed.isEmpty else {
+            return completion(.empty)
+        }
+        
+        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
+        cache = Cache(feed: feed, timestamp: timestamp)
+        completion(nil)
     }
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
@@ -26,19 +38,21 @@ class RuntimeFeedStore: FeedStore {
 class RuntimeFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = RuntimeFeedStore()
+        let sut = makeSUT()
         
         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = RuntimeFeedStore()
+        let sut = makeSUT()
         
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
@@ -75,5 +89,16 @@ class RuntimeFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_storeSideEffects_runSerially() {
         
+    }
+    
+    // MARK:- Helpers
+    
+    private func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> FeedStore {
+        let sut = RuntimeFeedStore()
+        trackMemoryLeaks(sut)
+        return sut
     }
 }
