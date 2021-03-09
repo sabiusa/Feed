3fb95af35598476e99cccbb938ba2662399c59a3
sabiusa <sabakhuts@gmail.com>
2021-03-09 15:16:47 +0400

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` overrides items saved by separate instances, proving we correctly manage data models on disk

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -43,6 +43,30 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         expect(sutToLoad, toLoad: feed)
     }
     
+    func test_save_overridesItemsSavedOnAnotherInstance() {
+        let sutToPerformFirstSave = makeSUT()
+        let sutToPerformLastSave = makeSUT()
+        let sutToLoad = makeSUT()
+        let firstFeed = uniqueImageFeed().models
+        let latestFeed = uniqueImageFeed().models
+        
+        let saveExp1 = expectation(description: "Wait for save completion")
+        sutToPerformFirstSave.save(firstFeed) { error in
+            XCTAssertNil(error, "Expected to save successfully")
+            saveExp1.fulfill()
+        }
+        wait(for: [saveExp1], timeout: 1.0)
+        
+        let saveExp2 = expectation(description: "Wait for save completion")
+        sutToPerformLastSave.save(latestFeed) { error in
+            XCTAssertNil(error, "Expected to save successfully")
+            saveExp2.fulfill()
+        }
+        wait(for: [saveExp2], timeout: 1.0)
+        
+        expect(sutToLoad, toLoad: latestFeed)
+    }
+    
     // MARK:- Helpers
     
     private func makeSUT(
