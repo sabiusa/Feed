b7fc7adb8349ab44dce680af12bd6ec800bed3ac
sabiusa <sabakhuts@gmail.com>
2021-03-14 21:59:20 +0400

    Extract FeedViewController test helpers into a separate file

---
+++ EssentialFeed/EssentialFeediOSTests/Controllers/Helpers/FeedViewController+TestHelpers.swift
@@ -0,0 +1,64 @@
+//
+//  FeedViewController+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Saba Khutsishvili on 3/14/21.
+//
+
+import UIKit
+import EssentialFeediOS
+
+extension FeedViewController {
+    
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+    
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
+        return feedImageView(at: index) as? FeedImageCell
+    }
+    
+    func simulateFeedImageViewNotVisible(at row: Int) {
+        let view = simulateFeedImageViewVisible(at: row)
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImageSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+    }
+    
+    func simulateFeedImageViewNearVisible(at row: Int) {
+        let pds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImageSection)
+        pds?.tableView(tableView, prefetchRowsAt: [index])
+    }
+    
+    func simulateFeedImageViewNotNearVisible(at row: Int) {
+        simulateFeedImageViewNearVisible(at: row)
+        
+        let pds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImageSection)
+        pds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
+}
+    
+extension FeedViewController {
+    
+    private var feedImageSection: Int {
+        return 0
+    }
+    
+    func numberOfRenderedFeedImageViews() -> Int {
+        return tableView.numberOfRows(inSection: feedImageSection)
+    }
+    
+    func feedImageView(at row: Int) -> UITableViewCell? {
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: feedImageSection)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
+}
